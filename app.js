8526af0e91cc2f89957351818ccee301b789a7e9
Francisco de Assis <chicoalive@gmail.com>
2024-07-23 14:07:25 -0300

    Backend - part2

--- app.js
+++ app.js
@@ -7,17 +7,52 @@ let textoCodificado = "";
 for (let i = 0; i < palavraNormal.length; i++) {
     let letra = palavraNormal[i];
     if (letra == "e") {
-        textoCodificado += "enter";
+        textoCodificado += "enter ";
     } else if (letra == "i") {
-        textoCodificado += "imes";
+        textoCodificado += "imes ";
     } else if (letra == "a") {
-        textoCodificado += "ai";
+        textoCodificado += "ai ";
     } else if (letra == "o") {
-        textoCodificado += "ober";
+        textoCodificado += "ober ";
     } else if (letra == "u") {
-        textoCodificado += "ufat";
+        textoCodificado += "ufat ";
     } else {
         textoCodificado += letra;
     }
 }
+
 alert(textoCodificado);
+
+var palavraCodificada = prompt("Digite o texto codificado");
+
+let textoDescodificado = "";
+
+let decodificar = {
+    "enter": "e",
+    "imes": "i",
+    "ai": "a",
+    "ober": "o",
+    "ufat": "u"
+};
+
+let palavra = "";
+for (let i = 0; i < palavraCodificada.length; i++) {
+    let letra2 = palavraCodificada[i];
+    if (letra2 != " ") {
+        palavra += letra2;
+    } else {
+        if (decodificar[palavra] != undefined) {
+            textoDescodificado += decodificar[palavra];
+        } else {
+            textoDescodificado += palavra;
+        }
+        palavra = "";
+    }
+}
+if (decodificar[palavra] != undefined) {
+    textoDescodificado += decodificar[palavra];
+} else {
+    textoDescodificado += palavra;
+}
+
+alert(textoDescodificado);
