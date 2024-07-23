fe6be558425768c3129dbaa0e6a49d51e35c3621
Francisco de Assis <chicoalive@gmail.com>
2024-07-23 14:59:20 -0300

    ligando back com front

--- app.js
+++ app.js
@@ -1,58 +1,60 @@
+function criptografar() {
+    var palavraNormal = prompt("Digite seu texto");
 
+    let textoCodificado = "";
 
-var palavraNormal = prompt("Digite seu texto");
-
-let textoCodificado = "";
-
-for (let i = 0; i < palavraNormal.length; i++) {
-    let letra = palavraNormal[i];
-    if (letra == "e") {
-        textoCodificado += "enter ";
-    } else if (letra == "i") {
-        textoCodificado += "imes ";
-    } else if (letra == "a") {
-        textoCodificado += "ai ";
-    } else if (letra == "o") {
-        textoCodificado += "ober ";
-    } else if (letra == "u") {
-        textoCodificado += "ufat ";
-    } else {
-        textoCodificado += letra;
-    }
-}
-
-alert(textoCodificado);
-
-var palavraCodificada = prompt("Digite o texto codificado");
-
-let textoDescodificado = "";
-
-let decodificar = {
-    "enter": "e",
-    "imes": "i",
-    "ai": "a",
-    "ober": "o",
-    "ufat": "u"
-};
-
-let palavra = "";
-for (let i = 0; i < palavraCodificada.length; i++) {
-    let letra2 = palavraCodificada[i];
-    if (letra2 != " ") {
-        palavra += letra2;
-    } else {
-        if (decodificar[palavra] != undefined) {
-            textoDescodificado += decodificar[palavra];
+    for (let i = 0; i < palavraNormal.length; i++) {
+        let letra = palavraNormal[i];
+        if (letra == "e") {
+            textoCodificado += "enter ";
+        } else if (letra == "i") {
+            textoCodificado += "imes ";
+        } else if (letra == "a") {
+            textoCodificado += "ai ";
+        } else if (letra == "o") {
+            textoCodificado += "ober ";
+        } else if (letra == "u") {
+            textoCodificado += "ufat ";
         } else {
-            textoDescodificado += palavra;
+            textoCodificado += letra;
         }
-        palavra = "";
     }
-}
-if (decodificar[palavra] != undefined) {
-    textoDescodificado += decodificar[palavra];
-} else {
-    textoDescodificado += palavra;
+
+    document.getElementById('mensagem').innerText = textoCodificado;
 }
 
-alert(textoDescodificado);
+function descriptografar() {
+    var palavraCodificada = prompt("Digite o texto codificado");
+
+    let textoDescodificado = "";
+
+    let decodificar = {
+        "enter": "e",
+        "imes": "i",
+        "ai": "a",
+        "ober": "o",
+        "ufat": "u"
+    };
+
+    let palavra = "";
+    for (let i = 0; i < palavraCodificada.length; i++) {
+        let letra2 = palavraCodificada[i];
+        if (letra2 != " ") {
+            palavra += letra2;
+        } else {
+            if (decodificar[palavra] != undefined) {
+                textoDescodificado += decodificar[palavra];
+            } else {
+                textoDescodificado += palavra;
+            }
+            palavra = "";
+        }
+    }
+    if (decodificar[palavra] != undefined) {
+        textoDescodificado += decodificar[palavra];
+    } else {
+        textoDescodificado += palavra;
+    }
+
+    document.getElementById('mensagem').innerText = textoDescodificado;
+}
